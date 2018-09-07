b07a2524983fe8549d9284038bd230a1852fdcd7
Dominic Carrington <carringtondominic@gmail.com>
2018-09-07 17:43:03 +0100

    Added comments

--- src/Event/EventDictionary.php
+++ src/Event/EventDictionary.php
@@ -1,7 +1,17 @@
 <?php
 namespace OpenPress\Event;
 
+/**
+ * Defines all the names of events
+ * Events can have three phrases:
+ * - pre => Before the instance is created
+ * - on => During the instance is being created
+ * - post => After the instance has been created
+ */
 class EventDictionary
 {
-    const ROUTE = "route";
+    /**
+     * Event is called when routes need to be defined.
+     */
+    const ROUTE = "route.on";
 }
