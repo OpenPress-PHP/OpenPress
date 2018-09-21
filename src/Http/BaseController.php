5a2d42d7a8e973a5ac6189b4e369d2e18ee4001a
Dominic Carrington <carringtondominic@gmail.com>
2018-09-21 22:13:29 +0100

    Added router to base controller

--- src/Http/BaseController.php
+++ src/Http/BaseController.php
@@ -1,14 +1,17 @@
 <?php
 namespace OpenPress\Http;
 
+use Slim\Router;
 use Slim\Views\Twig;
 
 class BaseController
 {
+    protected $router;
     protected $twig;
 
-    public function __construct(Twig $twig)
+    public function __construct(Router $router, Twig $twig)
     {
+        $this->router = $router;
         $this->twig = $twig;
     }
 }
